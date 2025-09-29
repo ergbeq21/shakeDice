4cdef6ebeafb083582e6588605c2a30991ae8068
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-09-29 10:23:36 +0200

    third commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,41 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount, onDestroy } from "svelte";
+    import { browser } from "$app/environment";
 
-    let number = null;
-    let lastShake = 0;
+	let number = "-";
+	let lastShake = 0;
 
-    function roll() {
-        number = Math.floor(Math.random() * 6) + 1;
-        document.getElementById("result").textContent = number;
-    }
+	function roll() {
+		number = Math.floor(Math.random() * 6) + 1;
+	}
 
-    onMount(()=>{
-         window.addEventListener("devicemotion", (event) => {
+	onMount(() => {
+		window.addEventListener("devicemotion", (event)=>{
             const acc = event.accelerationIncludingGravity;
             if (!acc) return;
 
             const totalAcc = Math.sqrt(
-                acc.x * acc.x + acc.y * acc.y + acc.z * acc.z
+                (acc.x || 0) * (acc.x || 0) +
+                (acc.y || 0) * (acc.y || 0) +
+                (acc.z || 0) * (acc.z || 0)
             );
 
             const now = Date.now();
 
-            if (totalAcc > 15) {
-                if (now - lastShake > 1000) {
-                    console.log("shake detected");
-                    lastShake = now;
-                    roll();
-                }
+            if (totalAcc > 15 && now - lastShake > 1000) {
+                console.log("shake detected");
+                lastShake = now;
+                roll();
             }
         });
-    })
-   
+	});
+
+	onDestroy(() => {
+		browser && window.removeEventListener("devicemotion", handleMotion);
+	});
 </script>
 
 <div>
-  <h1> Shake Dice </h1>
-  <p id="result">-</p>
+  <h1>Shake Dice</h1>
+  <p>{number}</p>
 </div>
